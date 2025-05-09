027afb17f62fd10ccd2f9aed8e6719546c0802a0
prada <sashabodiul07@gmail.com>
2025-05-10 00:52:48 +0300

    change redirect

--- src/middleware.ts
+++ src/middleware.ts
@@ -32,7 +32,7 @@ export function middleware(request: NextRequest) {
   
   // Если путь корневой админки, перенаправляем на страницу добавления бронирования
   if (pathname === '/admin' && userCookie) {
-    return NextResponse.redirect(new URL('/admin/bookings/add', request.url));
+    return NextResponse.redirect(new URL('/admin/dashboard', request.url));
   }
   
   return NextResponse.next();
